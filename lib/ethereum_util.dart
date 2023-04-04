400cb11eedfba21e88cea9df820b531b2dc35988
peipeiTu <37138187+peipeiTu@users.noreply.github.com>
2023-04-04 12:16:40 +0800

    fix: Export typed data constants.

--- lib/ethereum_util.dart
+++ lib/ethereum_util.dart
@@ -1,15 +1,16 @@
 library ethereum_util;
 
 export 'package:ethereum_util/src/abi.dart';
-export 'package:ethereum_util/src/utils/bigint.dart';
-export 'package:ethereum_util/src/utils/bytes.dart';
 export 'package:ethereum_util/src/constants.dart';
 export 'package:ethereum_util/src/eip1559Transaction.dart';
-export 'package:ethereum_util/src/utils/hash.dart';
 export 'package:ethereum_util/src/legacyTransaction.dart';
 export 'package:ethereum_util/src/random.dart';
 export 'package:ethereum_util/src/rlp.dart';
 export 'package:ethereum_util/src/signature.dart';
 export 'package:ethereum_util/src/transaction.dart';
+export 'package:ethereum_util/src/typed_data/constants.dart';
 export 'package:ethereum_util/src/typed_data/typed_data.dart';
+export 'package:ethereum_util/src/utils/bigint.dart';
+export 'package:ethereum_util/src/utils/bytes.dart';
+export 'package:ethereum_util/src/utils/hash.dart';
 export 'package:ethereum_util/src/utils/utils.dart';
